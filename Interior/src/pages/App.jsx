271790739d9fcaf8d32865ba61b1081963672287
Nataniel Junior <fnatanieljunior@gmail.com>
2023-08-20 19:30:36 -0300

    Iniciando projeto

---
+++ Interior/src/pages/App.jsx
@@ -0,0 +1,23 @@
+import '../style/App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <header>
+        <p>This Interior</p>
+        <nav>
+          <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          </ul>
+          </nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
